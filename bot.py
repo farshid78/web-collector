b552dd726636313056608a3bf628211249c6fa2d
Farshid78 <farshid.nabipour78@gmail.com>
2026-05-31 00:19:44 +0330

    Add bot workflow and auto user update

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ load_dotenv()
 
 from telethon import TelegramClient, events
 import os
+import subprocess
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
@@ -10,22 +11,27 @@ BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 client = TelegramClient("bot_session", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-# -----------------------------
-# /start → ثبت کاربر
-# -----------------------------
 @client.on(events.NewMessage(pattern="/start"))
 async def start(event):
     chat_id = event.chat_id
 
-    # ذخیره chat_id در فایل
+    # ذخیره chat_id
     with open("users.txt", "a") as f:
         f.write(str(chat_id) + "\n")
 
+    # پیام خوش‌آمد
     await event.respond(
         "سلام! 👋\n"
         "شما با موفقیت ثبت شدید.\n"
         "هر ۱۵ دقیقه آخرین کانفیگ‌ها برای شما ارسال می‌شود."
     )
+
+    # commit و push اتوماتیک
+    subprocess.run(["git", "config", "--global", "user.name", "github-actions"])
+    subprocess.run(["git", "config", "--global", "user.email", "actions@github.com"])
+    subprocess.run(["git", "add", "users.txt"])
+    subprocess.run(["git", "commit", "-m", "Add new user"])
+    subprocess.run(["git", "push"])
 
 print("ربات فعال شد...")
 client.run_until_disconnected()
